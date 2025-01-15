f89277e67416a17211e4dd54d36bc162d266bc3e
Chlybkie <kuba.bialy5@wp.pl>
2025-01-15 23:18:38 +0100

    picking up swing possibilities

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        TripStop trip = new TripStop("sdf ", " sdf", " sdf", 31, 12, 2025);
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+        } catch(Exception ignored){}
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                TravelPlaner travelPlaner = new TravelPlaner();
+                travelPlaner.setVisible(true);
+
+
+
+            }
+        });
+
     }
 }
---
+++ src/TravelPlaner.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TravelPlaner extends JFrame{
+    private JPanel contentPane;
+    private JButton hiButton;
+    private SomeButtonListener someButtonListener;
+
+    public TravelPlaner(){
+
+        setTitle("Travel Planer");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(contentPane);
+        setLocationRelativeTo(null);
+        setSize(500,500);
+        hiButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I was clicked!");
+            }
+        });
+
+        setVisible(true);
+
+
+
+
+
+    }
+
+}
